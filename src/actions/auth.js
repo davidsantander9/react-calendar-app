1fcc4c1c0885d121637b56b04d71640ffc6e1293
David <davidsantander9@gmail.com>
2021-07-28 21:07:16 -0500

    logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -73,9 +73,19 @@ export const startCheckin= () => {
     }
 }
 
-const checkingFinish = () => ({ type: types.authChekingFinish })
+const checkingFinish = () => ({ type: types.authChekingFinish });
 
 const login = ( user ) => ({
     type: types.authLogin,
     payload: user
-})
+});
+
+
+export const startLogout = () => {
+    return ( dispatch ) => {
+        localStorage.clear();
+        dispatch( logout() );
+    }
+}
+
+const logout = () => ({ type: types.authLogout })
