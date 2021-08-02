f3f34f50bced5cfb71a1dd890a72a9c59d1ee816
David <davidsantander9@gmail.com>
2021-08-02 13:54:43 -0500

    test on types

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,8 @@
 import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'enzyme-to-json';
 
-import { renderHook } from '@testing-library/react-hooks';
+// import { renderHook } from '@testing-library/react-hooks';
 
 
 Enzyme.configure({ adapter: new Adapter() });
---
+++ src/tests/types/types.test.js
@@ -0,0 +1,33 @@
+import { types } from "../../types/types"
+
+describe('Test on Types', () => {
+    test('types should be equal', () => {
+        
+        expect( types ).toEqual({
+
+            uiOpenModal: '[ui] Open modal',
+            uiCloseModal: '[ui] Close modal',
+        
+            eventStartAddNew: '[event] Start Add new',
+            eventAddNew: '[event] Add new',
+            eventSetActive: '[event] Set Active',
+            eventLogout: '[event] Logout event',
+            eventClearActiveEvent: '[event] Clear active event',
+            eventUpdated: '[event] Event updated',
+            eventDeleted: '[event] Event deleted',
+            eventLoaded: '[event] Event loaded',
+        
+            authCheking: '[auth] Checking login state',
+            authChekingFinish: '[auth] Finish checking login state',
+            authStartLogin: '[auth] Start login',
+            authLogin: '[auth] login',
+            authStartRegister: '[auth] Start register',
+            authStartTokinRenew: '[auth] Start tokin Renew', 
+            authLogout: '[auth] Logout',  
+        
+        
+        })
+
+    })
+    
+})
