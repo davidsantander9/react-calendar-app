b05fae1ba5ed5cc5fefed92bac5b747d20f0c41a
David <davidsantander9@gmail.com>
2021-08-02 14:28:31 -0500

    fetch withToken test

--- src/tests/helpers/fetch.test.js
+++ src/tests/helpers/fetch.test.js
@@ -1,14 +1,31 @@
-import { fetchWithoutToken } from "../../helpers/fetch";
+import { fetchWithoutToken, fetchWithToken } from "../../helpers/fetch";
 
 describe('Test on fetch helper', () => {
     
+    let token = ''
+
     test('fetchWithoutToken should work', async() => {
         const resp = await fetchWithoutToken('auth', { email: 'david@gmail.com', password: 'abc123' }, 'POST' )
 
+        expect( resp instanceof Response ).toBe(true);
+        
         const body = await resp.json();
         expect( body.ok ).toBe(true);
-        // expect( resp instanceof Response ).toBe(true);
-    })
+
+        token = body.token;
+
+    });
+
+    test('fetchWithToken should work', async() => {
+
+
+        localStorage.setItem('token', token);
+        
+        const resp = await fetchWithToken('events/', {}, 'GET' )
+        const body = await resp.json();
+        expect( body.msg ).toBe('get events');
+
+    });
     
 
 });
