ce45c013f78a281322a6937f1c2d783970992279
David <davidsantander9@gmail.com>
2021-08-02 19:11:37 -0500

    test call eventStartDelete

--- src/tests/components/ui/DeleteEventFab.test.js
+++ src/tests/components/ui/DeleteEventFab.test.js
@@ -5,7 +5,13 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
+
 import { DeleteEventFab } from '../../../components/ui/DeleteEventFab';
+import { eventStartDeleted } from '../../../actions/events';
+
+jest.mock('../../../actions/events', () => ({
+    eventStartDeleted: jest.fn()
+}))
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
@@ -26,6 +32,15 @@ describe('test on <DeleteEventFab/>', () => {
     test('should show correctly', () => {
         expect( wrapper ).toMatchSnapshot();
     });
+
+    test('should call eventStartDElete when click', () => {
+        
+        wrapper.find('button').prop('onClick')();
+
+        expect( eventStartDeleted ).toHaveBeenCalled()
+
+    })
+    
     
 
 })
